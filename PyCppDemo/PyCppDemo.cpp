777d8103ce9da3db9da7ebc908aa2657d7528968
zhwa <wangzhe0543@gmail.com>
2021-06-03 22:39:19 -0700

    update opaque example

--- PyCppDemo/PyCppDemo.cpp
+++ PyCppDemo/PyCppDemo.cpp
@@ -3,6 +3,8 @@
 
 namespace py = pybind11;
 
+PYBIND11_MAKE_OPAQUE(std::vector<Fraction>);
+
 PYBIND11_MODULE(PyCppDemo, m)
 {
 	m.def("square", &square, "Given a list of float numbers, return a list of float numbers in which each input element is squared.");
@@ -17,4 +19,16 @@ PYBIND11_MODULE(PyCppDemo, m)
 		.def("mul", &Fraction::mul, "Multiply the faction by given value.")
 		.def("__repr__", [](const Fraction& f) { return f.str(); })
 		.def("print", &Fraction::print);
+
+	// Opaque example: https://pybind11.readthedocs.io/en/stable/advanced/cast/stl.html#making-opaque-types.
+	py::class_<std::vector<Fraction>>(m, "FractionList")
+		.def(py::init<>())
+		.def("clear", &std::vector<Fraction>::clear)
+		.def("pop_back", &std::vector<Fraction>::pop_back)
+		.def("append", (void(std::vector<Fraction>::*)(const Fraction&)) &std::vector<Fraction>::push_back)
+		.def("__getitem__", (Fraction&(std::vector<Fraction>::*)(size_t)) &std::vector<Fraction>::at, py::return_value_policy::reference_internal)
+		.def("__len__", [](const std::vector<Fraction>& v) { return v.size(); })
+		.def("__iter__", [](std::vector<Fraction>& v) { return py::make_iterator(v.begin(), v.end()); }, py::keep_alive<0, 1>());
+
+	m.def("mul_fraction", &mul_fraction, "Multiply each faction in the list");
 }
